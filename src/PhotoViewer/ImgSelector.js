5c4b8f03894c7b8884bb8fce35369b3107672d6b
RoshniCh <roshni.chandrasekar@softwire.com>
2021-02-08 18:47:09 +0000

    displays clicked image on top using imageselector - formatted

--- src/PhotoViewer/ImgSelector.js
+++ src/PhotoViewer/ImgSelector.js
@@ -4,17 +4,21 @@ import { ImageUrls } from './getImageUrls.js';
 
 function ImgSelector() {  
     const urlList = ImageUrls;
-    const [clickedImage, setClickedImage] = useState(0); 
+    const [clickedImage, setClickedImage] = useState(urlList[0]); 
     
     // function incrementCount() {
     //     setClickCount(e => setMessage(e.target.src)) }
 
     return (                
-        <div>    
-                You clicked <img src = {clickedImage}></img><br></br><br></br>
+        <div>   
+            <br></br><br></br> 
+                <center>
+                <img class = "image-unselected" src = {clickedImage} width = "600" height = "400"></img> </center> <br></br><br></br>
+                <center>
                 {urlList.map(urls => (
                     <img class = "image-unselected" src = {urls} onClick={(e => setClickedImage(e.target.src)) }></img>
                 ))}
+                </center>
         </div>
     );
 }
--- src/PhotoViewer/getImageUrls.js
+++ src/PhotoViewer/getImageUrls.js
@@ -8,7 +8,7 @@ function getImageUrls() {
     for (let i = 0; i < 50; i++) {
         if (!brokenImages.includes(i)) {
             const imageNumberString = i.toString().padStart(2, '0');
-            urls.push(`https://picsum.photos/id/6${imageNumberString}/300/200.jpg`)
+            urls.push(`https://picsum.photos/id/6${imageNumberString}/250/200.jpg`)
         }
     }
 
